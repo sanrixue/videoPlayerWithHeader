a7d10d304b1c41cd05f59a7d159e277074700381
sanrixue <1040811211@qq.com>
2019-09-23 13:42:25 +0800

    add:video_player pub

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/xueyujing/Desktop/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/xueyujing/Desktop/flutterDemo/FlutterPlugin/video_player-0.10.2+1/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/xueyujing/Desktop/Flutter/flutter/bin/cache/artifacts/engine/ios"
